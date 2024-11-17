e3dfad28abc2627a56ef0be7dcef36e33c228799
Arseni Kavalchuk <ars.kov@gmail.com>
2024-11-17 12:41:14 +0000

    Initial commit of KMP project template

---
+++ kmp-lib/README.md
@@ -0,0 +1,4 @@
+# Kotlin Multiplatform Library Template
+
+This repository contains a simple library project, intended to demonstrate a [Kotlin Multiplatform](https://kotlinlang.org/docs/multiplatform.html) library integrated with iOS application
+
---
+++ kmp-lib/library/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+group = "io.github.arskov"
+version = "1.0.0"
+
+val iosXCFrameworkName = "KmpLib"
+val binaryName = "kmp-lib"
+
+
+kotlin {
+
+    androidTarget {
+        publishLibraryVariants("release")
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
+
+    val xcf = XCFramework(iosXCFrameworkName)
+    val iosTargets = listOf(iosX64(), iosArm64(), iosSimulatorArm64())
+    iosTargets.forEach {
+        it.binaries.framework {
+            baseName = binaryName
+            xcf.add(this)
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                //put your multiplatform dependencies here
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "io.github.arskov"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
